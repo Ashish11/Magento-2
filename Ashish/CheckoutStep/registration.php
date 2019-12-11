dd51c85317d8d9f43c532828889cd080fffaee05
Ashish Ranade <ranade.ashish11@gmail.com>
2019-12-11 15:43:26 +0530

    Check Out Step
    Checkout step module add new step into the checkout process, in future, I will add functionality to show calender to selct the desired delivery date.

---
+++ Ashish/CheckoutStep/registration.php
@@ -0,0 +1,13 @@
+<?php
+
+/*
+ * Created By: Ashish Ranade On : 11 Dec, 2019 1:53:32 PM
+ * Project: Training
+ * File: registration.php
+ */
+
+use \Magento\Framework\Component\ComponentRegistrar;
+
+ComponentRegistrar::register(ComponentRegistrar::MODULE,
+        'Ashish_CheckoutStep',
+        __DIR__);
