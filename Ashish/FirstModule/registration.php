de8fa77bba236a3c84c2ee82f4685035373b1fb8
Ashish Ranade <ranade.ashish11@gmail.com>
2020-03-16 19:13:34 +0530

    Getting Started With Magento 2 Module Development
    Getting Started With Magento 2 Module Development

---
+++ Ashish/FirstModule/registration.php
@@ -0,0 +1,12 @@
+<?php
+
+/*
+ * Created By: Ashish Ranade On : 14 Feb 2020 3:39:59 PM
+ * Project: ASolutions
+ * File: registration.php
+ */
+
+use \Magento\Framework\Component\ComponentRegistrar;
+
+ComponentRegistrar::register(ComponentRegistrar::MODULE, 'Ashish_FirstModule',
+        __DIR__);
